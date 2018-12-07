29d8c2860809bd14abb063350dc3a52ffac5d7a6
roman93ts <netsky93@gmail.com>
2018-12-08 00:36:00 +0300

    Updates

---
+++ precache-manifest.5316cb0bb022ec27481b10afb8709881.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49d69044dcc19f0c7c57",
+    "url": "/robots/static/js/runtime~main.49d69044.js"
+  },
+  {
+    "revision": "6c0db8fc9332a39c460f",
+    "url": "/robots/static/js/main.6c0db8fc.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/robots/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "6c0db8fc9332a39c460f",
+    "url": "/robots/static/css/main.c4287f5a.chunk.css"
+  },
+  {
+    "revision": "8244fb96802ed49fc50d0fe6722b9b33",
+    "url": "/robots/index.html"
+  }
+];
