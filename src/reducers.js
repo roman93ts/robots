4ff85f56f8788290624d03994093182d8d928f96
roman93ts <netsky93@gmail.com>
2018-12-11 01:46:04 +0300

    lil changes in reducers

--- src/reducers.js
+++ src/reducers.js
@@ -7,7 +7,7 @@ const initialState = {
 export const changeRobots = (state=initialState, action={}) => {
 	switch(action.type){
 		case CHANGE_SEARCH_FIELD:
-			return Object.assign({},searchField,{searchField:action.payload});
+			return Object.assign({},state,{searchField:action.payload});
 		default:
 			return state;
 	}
