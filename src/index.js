45075a6bc97c276134470f21362b23ed56083d49
roman93ts <netsky93@gmail.com>
2018-12-12 21:35:13 +0300

    create Provider component

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
+import { changeRobots } from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const store = createStore(changeRobots);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(<Provider store={store}>
+					<App />
+				</Provider>
+				, document.getElementById('root'));
+
+
 serviceWorker.unregister();
